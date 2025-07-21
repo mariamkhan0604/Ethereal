70a63577c8f7accaa7e838847f6b8f0111ede7bf
Mariam Khan <mariamkhan0604@gmail.com>
2025-07-21 17:33:37 +0530

    added register page and linked it to the navbar

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,48 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const path = require("path");
 const engine = require("ejs-mate");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const mongoose = require("mongoose");
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.use(express.json());
+console.log("MongoDB URL:", process.env.DATABASE_URL); // Debug line
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB", mongoose.connection.host))
+.catch(err => {
+  console.error("MongoDB connection failed:");
+  console.error(err.message); // cleaner output
+  process.exit(1); // Optional: Exit app on DB failure
+});
+
 app.engine("ejs", engine);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../frontend/views"));
 app.use(express.static(path.join(__dirname, "../frontend/public")));
+
+const sessionConfig = {
+  secret: process.env.SESSION_SECRET ,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.DATABASE_URL
+  }),
+  cookie: {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+  }
+};
+const authRoutes = require('./routes/auth');
+app.use('/auth', authRoutes); 
+
+app.use(session(sessionConfig));
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -20,6 +55,12 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
   res.render("contact");
 });
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.session.user ;
+  next();
+});
+
 const port = 3000;
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on http://0.0.0.0:${port}`);
---
+++ backend/routes/auth.js
@@ -0,0 +1,22 @@
+const express= require('express')
+const router=express.Router();
+const User=require('../models/User');
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+
+router.get('/register',(req,res)=>{
+    res.render('auth/register');
+});
+router.post('/register',catchAsync(async (req, res) => {
+      const { firstName, lastName, email, password } = req.body;
+       const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        throw new ExpressError('Email already registered.', 400);
+    }
+    const newUser = new User({ firstName, lastName, email, password });
+    await newUser.save();
+    console.log("✅ User saved:", newUser);
+    res.redirect('/');
+
+}));
+module.exports=router;
