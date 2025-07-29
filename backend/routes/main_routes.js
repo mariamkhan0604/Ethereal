c6bda0c8b6d051f92739a2f526ac58d33c7b9e1a
Mariam Khan <mariamkhan0604@gmail.com>
2025-07-29 19:03:50 +0530

    handled shop page-cart conflicts

--- backend/routes/main_routes.js
+++ backend/routes/main_routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product.js");
 const Order = require("../models/Order.js");
-
+const Category = require('../models/Category');
 //Home route
 router.get("/", (req, res) => {
   res.render("home");
@@ -17,9 +17,58 @@ router.get("/contact", (req, res) => {
 });
 
 //Shop routes
-router.get("/shop", async (req, res) => {
-  const products = await Product.find({});
-  res.render("shop", { products });
+router.get("/shop", async (req, res,next) => {
+  try {
+        const minPrice = parseFloat(req.query.minPrice) || 0;
+        const maxPrice = parseFloat(req.query.maxPrice) || 5000; 
+
+        const frontendSelectedCategoryNames = req.query.category
+            ? (Array.isArray(req.query.category) ? req.query.category : [req.query.category])
+            : [];
+
+    
+        const allCategoriesInDb = await Category.find({}); 
+
+    
+        const categoryNameToIdMap = {};
+        for (const cat of allCategoriesInDb) {
+            categoryNameToIdMap[cat.name] = cat._id;
+        }
+
+      
+        const backendSelectedCategoryIds = frontendSelectedCategoryNames
+            .map(name => categoryNameToIdMap[name]) 
+            .filter(id => id); 
+
+     
+        let filter = {};
+
+  
+        filter.price = {
+            $gte: minPrice,
+            $lte: maxPrice
+        };
+
+     
+        if (backendSelectedCategoryIds.length > 0) {
+            filter.category = { $in: backendSelectedCategoryIds };
+        }
+
+        const products = await Product.find(filter);
+
+      
+        res.render('shop', {
+            products: products,
+            minPrice: minPrice,
+            maxPrice: maxPrice,
+            categories: allCategoriesInDb.map(cat => cat.name), 
+            selectedCategories: frontendSelectedCategoryNames 
+        });
+
+    } catch (e) {
+        console.error("Error in /shop route:", e);
+        next(e);
+    }
 });
 
 module.exports = router;
